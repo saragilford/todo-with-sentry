8a53274ac35fa5f3df866582a46b5487a6d8b6f1
Sara Gilford <sara@sentry.io>
2017-11-15 13:24:30 -0800

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171113211342) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20171113211342) do
   create_table "tasks", force: :cascade do |t|
     t.string "description"
     t.boolean "completed"
-    t.integer "list_id"
+    t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_tasks_on_list_id"
